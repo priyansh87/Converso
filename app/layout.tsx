d3deaf17a2ed4d62e078c024233d826602404048
Priyansh87 <priyansh56701@gmail.com>
2025-06-01 18:41:36 +0530

    implement clerk auth , payment and companion databse actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
+
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
+
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
   subsets: ["latin"],
@@ -21,8 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>
-        {children}</body>
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+        </body>
     </html>
   );
 }
